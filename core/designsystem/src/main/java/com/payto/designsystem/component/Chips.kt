6e26cba7c19bfcd1c2fe2acb977b991f10a96133
anhahyoun <dksgkgus@gmail.com>
2025-01-12 17:17:19 +0900

    - 디자인 시스템 Chips 추가

---
+++ core/designsystem/src/main/java/com/payto/designsystem/component/Chips.kt
@@ -0,0 +1,41 @@
+package com.payto.designsystem.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.payto.designsystem.theme.Color
+import com.payto.designsystem.theme.typography
+
+@Composable
+fun Chips(modifier: Modifier, text: String) {
+    Box(
+        modifier = modifier
+            .background(
+                Color.Primary.normal,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .height(20.dp)
+            .padding(horizontal = 8.dp)
+    ) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = text,
+            style = typography.contentAccent,
+            color = Color.Inverse.label
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ChipsPreview() {
+    Chips(modifier = Modifier, text = "여행")
+}
