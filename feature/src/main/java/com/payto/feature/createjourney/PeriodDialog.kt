4b4d20ac722e352d6940862e9407d436eb46448f
anhahyoun <dksgkgus@gmail.com>
2025-05-16 00:00:32 +0900

    - 여정 생성 달력 오류 수정

--- feature/src/main/java/com/payto/feature/createjourney/PeriodDialog.kt
+++ feature/src/main/java/com/payto/feature/createjourney/PeriodDialog.kt
@@ -2,11 +2,14 @@
 
 package com.payto.feature.createjourney
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -21,17 +24,23 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.payto.designsystem.extension.rippleClickable
+import com.payto.designsystem.icon.IconPack
+import com.payto.designsystem.icon.iconpack.Close
 import com.payto.designsystem.theme.Color
 import com.payto.designsystem.theme.Component
 import com.payto.designsystem.theme.typography
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -43,24 +52,44 @@ fun PeriodDialog(
     modifier: Modifier = Modifier,
 ) {
     val state = rememberDateRangePickerState()
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    val coroutineScope = rememberCoroutineScope()
+
+    val hideAndOnDismissRequest: () -> Unit = {
+        coroutineScope
+            .launch { sheetState.hide() }
+            .invokeOnCompletion {
+                onDismissRequest.invoke(state.selectedStartDateMillis, state.selectedEndDateMillis)
+            }
+    }
+    LaunchedEffect(state.selectedEndDateMillis) {
+        if (state.selectedEndDateMillis != null) {
+            hideAndOnDismissRequest()
+        }
+    }
     ModalBottomSheet(
         modifier = modifier,
         onDismissRequest = {
-            onDismissRequest.invoke(state.selectedStartDateMillis, state.selectedEndDateMillis)
+            hideAndOnDismissRequest()
         },
         containerColor = Color.Inverse.label,
-        sheetState = rememberModalBottomSheetState(),
+        sheetState = sheetState,
         dragHandle = {
             BottomSheetDefaults.DragHandle(color = Component.Fill.strong)
         }
     ) {
-        PeriodDialogContent(state)
+        PeriodDialogContent(
+            state = state,
+            onDismissRequest = {
+                hideAndOnDismissRequest()
+            }
+        )
     }
 }
 
 
 @Composable
-fun PeriodDialogContent(state: DateRangePickerState) {
+fun PeriodDialogContent(state: DateRangePickerState, onDismissRequest: () -> Unit) {
     val dateRange by remember(state.selectedStartDateMillis, state.selectedEndDateMillis) {
         derivedStateOf {
             if (state.selectedEndDateMillis != null && state.selectedStartDateMillis != null) {
@@ -91,70 +120,87 @@ fun PeriodDialogContent(state: DateRangePickerState) {
     }
 
     DateRangePicker(
+        modifier = Modifier.fillMaxHeight(0.6f),
         state = state,
-        title = {
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
-                text = "여행기간 설정",
-                style = typography.highlightBold,
-                color = Color.Label.normal
-            )
-        },
+        title = null,
         headline = {
-            Row(
-                modifier = Modifier.padding(horizontal = 16.dp),
-                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(24.dp)
             ) {
-                if (dateRange != null) {
-                    Box(
-                        modifier = Modifier
-                            .height(28.dp)
-                            .background(
-                                color = Component.Fill.primary,
-                                shape = RoundedCornerShape(40.dp)
-                            )
-                            .border(
-                                width = 1.dp,
-                                color = Color.Primary.normal,
-                                shape = RoundedCornerShape(40.dp)
-                            )
-                            .padding(horizontal = 8.dp, vertical = 4.dp)
-                    ) {
-                        Text(
-                            modifier = Modifier.align(Alignment.Center),
-                            text = "$dateRange",
-                            style = typography.contentAccent,
-                            color = Color.Primary.normal,
-                        )
-                    }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "여행기간 설정",
+                        style = typography.highlightBold,
+                        color = Color.Label.normal
+                    )
+                    Image(
+                        modifier = Modifier.rippleClickable(onClick = onDismissRequest),
+                        imageVector = IconPack.Close,
+                        contentDescription = "닫기"
+                    )
                 }
-                if (range != null) {
-                    Box(
-                        modifier = Modifier
-                            .height(28.dp)
-                            .background(
-                                color = Component.Fill.primary,
-                                shape = RoundedCornerShape(40.dp)
-                            )
-                            .border(
-                                width = 1.dp,
+
+                Row(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    if (dateRange != null) {
+                        Box(
+                            modifier = Modifier
+                                .height(28.dp)
+                                .background(
+                                    color = Component.Fill.primary,
+                                    shape = RoundedCornerShape(40.dp)
+                                )
+                                .border(
+                                    width = 1.dp,
+                                    color = Color.Primary.normal,
+                                    shape = RoundedCornerShape(40.dp)
+                                )
+                                .padding(horizontal = 8.dp, vertical = 4.dp)
+                        ) {
+                            Text(
+                                modifier = Modifier.align(Alignment.Center),
+                                text = "$dateRange",
+                                style = typography.contentAccent,
                                 color = Color.Primary.normal,
-                                shape = RoundedCornerShape(40.dp)
                             )
-                            .padding(horizontal = 8.dp, vertical = 4.dp)
-                    ) {
-                        Text(
-                            modifier = Modifier.align(Alignment.Center),
-                            text = "$range",
-                            style = typography.contentAccent,
-                            color = Color.Primary.normal,
-                            textAlign = TextAlign.Center
-                        )
+                        }
+                    }
+                    if (range != null) {
+                        Box(
+                            modifier = Modifier
+                                .height(28.dp)
+                                .background(
+                                    color = Component.Fill.primary,
+                                    shape = RoundedCornerShape(40.dp)
+                                )
+                                .border(
+                                    width = 1.dp,
+                                    color = Color.Primary.normal,
+                                    shape = RoundedCornerShape(40.dp)
+                                )
+                                .padding(horizontal = 8.dp, vertical = 4.dp)
+                        ) {
+                            Text(
+                                modifier = Modifier.align(Alignment.Center),
+                                text = "$range",
+                                style = typography.contentAccent,
+                                color = Color.Primary.normal,
+                                textAlign = TextAlign.Center
+                            )
+                        }
                     }
                 }
             }
+
         },
         showModeToggle = false,
         colors = DatePickerDefaults.colors(
@@ -176,5 +222,5 @@ fun PeriodDialogContent(state: DateRangePickerState) {
 @Preview
 @Composable
 private fun PeriodDialogPreview() {
-    PeriodDialogContent(rememberDateRangePickerState())
+    PeriodDialogContent(rememberDateRangePickerState(), onDismissRequest = {})
 }
